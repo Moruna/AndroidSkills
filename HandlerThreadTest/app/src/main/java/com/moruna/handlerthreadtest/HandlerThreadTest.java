68c8b591d3428079f61b013d5667034357d34372
moruna <moruna.mo@dudu.tm>
2017-07-14 15:40:18 +0800

    HandlerThread使用

---
+++ HandlerThreadTest/app/src/main/java/com/moruna/handlerthreadtest/HandlerThreadTest.java
@@ -0,0 +1,50 @@
+package com.moruna.handlerthreadtest;
+
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Looper;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class HandlerThreadTest extends AppCompatActivity {
+
+    HandlerThread handlerThread;
+    ConcreteHandler concreteHandler;
+    String TAG = HandlerThreadTest.class.getSimpleName();
+    int value = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_handler_thread_test);
+        handlerThread = new HandlerThread("TestHandlerThread");
+        handlerThread.start();
+        concreteHandler = new ConcreteHandler(handlerThread.getLooper());
+        Log.d(TAG, Thread.currentThread() + "-- thread print --" + value);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Log.d(TAG, Thread.currentThread() + "-- thread print --" + value);
+                Message msg = new Message();
+                msg.what = value;
+                concreteHandler.sendMessage(msg);
+            }
+        }).start();
+    }
+
+    private class ConcreteHandler extends Handler {
+        private ConcreteHandler(Looper looper) {
+            //如果不super looper，则还会运行在主线程
+            super(looper);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            //可执行耗时操作
+            Log.d(TAG, Thread.currentThread() + "-- thread print --" + msg.what);
+        }
+    }
+}
